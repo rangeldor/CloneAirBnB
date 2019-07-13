c12644c508fb2bd2928605473d19c52fb8630ca5
Daniel Oliveira Rangel <contatomarketingcode@gmail.com>
2019-07-13 20:47:39 -0300

    Primeiro commit

---
+++ airbnb-server/app/Controllers/Http/PropertyController.js
@@ -0,0 +1,90 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with properties
+ */
+
+const Property = use('App/Models/Property')
+
+class PropertyController {
+  /**
+   * Show a list of all properties.
+   * GET properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request }) {
+    const { latitude, longitude } = request.all()
+
+    const properties = Property.query()
+      .nearBy(latitude, longitude, 10)
+      .fetch()
+
+    return properties
+  }
+
+  /**
+   * Create/save a new property.
+   * POST properties
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+  }
+
+  /**
+   * Display a single property.
+   * GET properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params }) {
+    const property = await Property.findOrFail(params.id)
+
+    await property.load('images')
+
+    return property
+  }
+
+
+  /**
+   * Update property details.
+   * PUT or PATCH properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {
+  }
+
+  /**
+   * Delete a property with id.
+   * DELETE properties/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, auth, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+  }
+}
+
+module.exports = PropertyController
