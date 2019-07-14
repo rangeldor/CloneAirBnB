6693045ae1c5ab5dba74fb95ac73df4062823d99
Daniel Oliveira Rangel <rangeldor@gmail.com>
2019-07-13 21:35:44 -0300

    Feito a conclusão da API com NodeJs (Adonis)

--- airbnb-server/app/Controllers/Http/PropertyController.js
+++ airbnb-server/app/Controllers/Http/PropertyController.js
@@ -24,6 +24,7 @@ class PropertyController {
     const { latitude, longitude } = request.all()
 
     const properties = Property.query()
+      .with('images')
       .nearBy(latitude, longitude, 10)
       .fetch()
 
@@ -38,7 +39,19 @@ class PropertyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {
+  async store({ auth, request, response }) {
+    const { id } = auth.user
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    const property = await Property.create({ ...data, user_id: id })
+
+    return property
   }
 
   /**
@@ -68,6 +81,21 @@ class PropertyController {
    * @param {Response} ctx.response
    */
   async update({ params, request, response }) {
+    const property = await Property.findOrFail(params.id)
+
+    const data = request.only([
+      'title',
+      'address',
+      'latitude',
+      'longitude',
+      'price'
+    ])
+
+    property.merge(data)
+
+    await property.save()
+
+    return property
   }
 
   /**
